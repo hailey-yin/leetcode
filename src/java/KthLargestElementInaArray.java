4843ca47af98e5f4c86755f9c5d0b6af32e6370c
Hailey Yin <hailinyi@usc.edu>
2017-10-29 20:18:04 -0700

    10/29

---
+++ src/java/KthLargestElementInaArray.java
@@ -0,0 +1,77 @@
+package java;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * Created by haileyyin on 10/29/17.
+ */
+public class KthLargestElementInaArray {
+    /*
+    simplest
+     */
+    public int findKthLargest1(int[] nums, int k) {
+        final int N = nums.length;
+        Arrays.sort(nums);
+        return nums[N - k];
+    }
+    /*
+    PriorityQueue
+    O(N lg K) running time + O(K) memory
+     */
+    public int findKthLargest2(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num:nums){
+            pq.offer(num);
+            if(pq.size()>k)
+                pq.poll();
+        }
+        return pq.peek();
+    }
+    /*
+    sorted the array first, then return the kth element.
+     */
+    public int findKthLargest(int[] nums, int k) {
+
+        k = nums.length - k;
+        int lo = 0;
+        int hi = nums.length - 1;
+        while (lo < hi) {
+            final int j = partition(nums, lo, hi);
+            if(j < k) {
+                lo = j + 1;
+            } else if (j > k) {
+                hi = j - 1;
+            } else {
+                break;
+            }
+        }
+        return nums[k];
+    }
+
+    private int partition(int[] a, int lo, int hi) {
+
+        int i = lo;
+        int j = hi + 1;
+        while(true) {
+            while(i < hi && less(a[++i], a[lo]));
+            while(j > lo && less(a[lo], a[--j]));
+            if(i >= j) {
+                break;
+            }
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+
+    private void exch(int[] a, int i, int j) {
+        final int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+
+    private boolean less(int v, int w) {
+        return v < w;
+    }
+}
